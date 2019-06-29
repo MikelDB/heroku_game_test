9ab350a2305b3a3347c3b53f71e7928ef47ca2b6
Mikel Díez Buil <mikeldiezbuil@gmail.com>
2019-06-30 01:29:53 +0200

    Working with node

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var path = require('path');
 
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static(__dirname + '/'));
 
 app.listen(port);
