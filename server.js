b08e831f1e8597d4ab9c7a769afd93f998c8dfd5
Mikel Díez Buil <mikeldiezbuil@gmail.com>
2019-06-30 01:24:26 +0200

    New server

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(port);
